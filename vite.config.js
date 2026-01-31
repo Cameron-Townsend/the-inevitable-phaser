9f892574fc38c573f4f7321dcea637ad42919bf2
Cameron-Townsend <cgt805@gmail.com>
2026-01-31 12:14:58 -0800

    Update allowedHosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
-    allowedHosts: ['.gitpod.dev'],
+    allowedHosts: ['https://5173--019c1583-5ee4-71e1-9fb1-dba07993029c.us-east-1-01.gitpod.dev'],
   },
 });
